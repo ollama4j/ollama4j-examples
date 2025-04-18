1a838db0b2e4cd45575e01dc2039e574e49421d7
amithkoujalgi <koujalgi.amith@gmail.com>
2025-04-18 18:43:11 +0530

    init

--- src/main/java/io/github/ollama4j/examples/GenerateEmbeddingsWithRequestModel.java
+++ src/main/java/io/github/ollama4j/examples/GenerateEmbeddingsWithRequestModel.java
@@ -4,10 +4,13 @@ import io.github.ollama4j.OllamaAPI;
 import io.github.ollama4j.exceptions.OllamaBaseException;
 import io.github.ollama4j.models.embeddings.OllamaEmbedRequestModel;
 import io.github.ollama4j.models.embeddings.OllamaEmbedResponseModel;
+import io.github.ollama4j.utils.OptionsBuilder;
 import io.github.ollama4j.utils.Utilities;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GenerateEmbeddingsWithRequestModel {
 
@@ -17,7 +20,10 @@ public class GenerateEmbeddingsWithRequestModel {
 
         OllamaAPI ollamaAPI = new OllamaAPI(host);
 
-        OllamaEmbedResponseModel embeddings = ollamaAPI.embed(new OllamaEmbedRequestModel("all-minilm", Arrays.asList("Why is the sky blue?", "Why is the grass green?")));
+        OllamaEmbedRequestModel requestModel = new OllamaEmbedRequestModel("all-minilm", Arrays.asList("Why is the sky blue?", "Why is the grass green?"));
+        requestModel.setOptions(new OptionsBuilder().setSeed(42).setTemperature(0.7f).build().getOptionsMap());
+
+        OllamaEmbedResponseModel embeddings = ollamaAPI.embed(requestModel);
 
         System.out.println(embeddings.getEmbeddings());
     }
