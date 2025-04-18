e41e907d37f98afd627e240e064ee53f818da0f0
amithkoujalgi <koujalgi.amith@gmail.com>
2025-04-18 12:09:17 +0530

    init

---
+++ src/main/java/io/github/ollama4j/examples/GenerateStreamingWithTokenConcatenation.java
@@ -0,0 +1,52 @@
+package io.github.ollama4j.examples;
+
+import io.github.ollama4j.OllamaAPI;
+import io.github.ollama4j.exceptions.OllamaBaseException;
+import io.github.ollama4j.models.generate.OllamaStreamHandler;
+import io.github.ollama4j.utils.OptionsBuilder;
+
+import java.io.IOException;
+
+public class GenerateStreamingWithTokenConcatenation {
+    public static void main(String[] args) throws OllamaBaseException, IOException, InterruptedException {
+
+        String host = "http://localhost:11434/";
+
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setVerbose(false);
+
+        // define a stream handler (Consumer<String>)
+        OllamaStreamHandler streamHandler = System.out::print;
+
+        new MyStreamingGenerator(ollamaAPI, streamHandler).start();
+    }
+}
+
+class MyStreamHandler implements OllamaStreamHandler {
+
+    @Override
+    public void accept(String message) {
+        System.out.print(message);
+    }
+}
+
+class MyStreamingGenerator extends Thread {
+    private final OllamaAPI ollamaAPI;
+    private OllamaStreamHandler streamHandler = new MyStreamHandler();
+
+    MyStreamingGenerator(OllamaAPI ollamaAPI, OllamaStreamHandler streamHandler) {
+        this.ollamaAPI = ollamaAPI;
+        this.streamHandler = streamHandler;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ollamaAPI.generate("llama3.2:1b",
+                    "Describe yourself in 10 words",
+                    true, new OptionsBuilder().build(), streamHandler);
+        } catch (OllamaBaseException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
