aaf8107e1e8e68bdb97c5a962eb82d6f28f229ce
amithkoujalgi <koujalgi.amith@gmail.com>
2025-04-18 15:32:45 +0530

    init

---
+++ src/main/java/io/github/ollama4j/examples/GenerateAsync.java
@@ -0,0 +1,35 @@
+package io.github.ollama4j.examples;
+
+import io.github.ollama4j.OllamaAPI;
+import io.github.ollama4j.models.response.OllamaAsyncResultStreamer;
+import io.github.ollama4j.types.OllamaModelType;
+
+public class GenerateAsync {
+
+    public static void main(String[] args) throws Exception {
+        String host = "http://localhost:11434/";
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setRequestTimeoutSeconds(60);
+        String prompt = "List all cricket world cup teams of 2019.";
+        OllamaAsyncResultStreamer streamer = ollamaAPI.generateAsync("llama3.2:1b", prompt, false);
+
+        // Set the poll interval according to your needs.
+        // Smaller the poll interval, more frequently you receive the tokens.
+        int pollIntervalMilliseconds = 1000;
+
+        while (true) {
+            String tokens = streamer.getStream().poll();
+            System.out.print(tokens);
+            if (!streamer.isAlive()) {
+                break;
+            }
+            Thread.sleep(pollIntervalMilliseconds);
+        }
+
+        System.out.println("\n------------------------");
+        System.out.println("Complete Response:");
+        System.out.println("------------------------");
+
+        System.out.println(streamer.getCompleteResponse());
+    }
+}
