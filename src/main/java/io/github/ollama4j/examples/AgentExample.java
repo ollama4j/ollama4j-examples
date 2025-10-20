bfb9d23f6857caad20fee74360e29b107fdd85e0
Amith Koujalgi <koujalgi.amith@gmail.com>
2025-10-20 19:31:51 +0530

    feat: add AgentExample to demonstrate usage of the Agent API with logging configuration

--- src/main/java/io/github/ollama4j/examples/AgentExample.java
+++ src/main/java/io/github/ollama4j/examples/AgentExample.java
@@ -14,13 +14,16 @@ import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 
-/** Example usage of the Agent API with some dummy tool functions. */
-public class SampleAgent {
+/**
+ * Example usage of the Agent API with some dummy tool functions.
+ */
+public class AgentExample {
     public static void main(String[] args) throws OllamaException {
         // Disable debug logging and enable only INFO for logback
         Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         rootLogger.setLevel(Level.INFO);
 
+        // This is using an agent yaml file from classpath, but you can also use a different file path.
         Agent agent = Agent.load("agent.yaml");
         agent.runInteractive();
     }
