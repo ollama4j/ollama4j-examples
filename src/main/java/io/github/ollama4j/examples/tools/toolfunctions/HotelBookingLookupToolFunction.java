d4a3d99355a4f98bb30ad28ebb8b9a4b40a7fad7
amithkoujalgi <koujalgi.amith@gmail.com>
2025-10-22 12:57:09 +0530

    refactor: remove CalculatorToolFunction and add HotelBookingLookupToolFunction for booking ID lookups in agent.yaml

---
+++ src/main/java/io/github/ollama4j/examples/tools/toolfunctions/HotelBookingLookupToolFunction.java
@@ -0,0 +1,16 @@
+package io.github.ollama4j.examples.tools.toolfunctions;
+
+import io.github.ollama4j.tools.ToolFunction;
+
+import java.util.Map;
+
+/**
+ * ToolFunction implementation for looking up booking by ID.
+ */
+public class HotelBookingLookupToolFunction implements ToolFunction {
+    @Override
+    public Object apply(Map<String, Object> arguments) {
+        String bookingID = (String) arguments.get("bookingId");
+        return String.format("Found a booking with ID: %s. Booked for 2 guests. Enjoy your stay", bookingID);
+    }
+}
