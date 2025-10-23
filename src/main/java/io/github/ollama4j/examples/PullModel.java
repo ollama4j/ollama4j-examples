d1514c04404cfbe265bcd6d9735c39769f3e28bb
amithkoujalgi <koujalgi.amith@gmail.com>
2025-10-23 09:23:21 +0530

    refactor

--- src/main/java/io/github/ollama4j/examples/PullModel.java
+++ src/main/java/io/github/ollama4j/examples/PullModel.java
@@ -7,13 +7,11 @@ public class PullModel {
 
     public static void main(String[] args) throws Exception {
 
-        String model = "mistral:7b";
-
         Ollama ollama = Utilities.setUp();
         // We're just using our quick-setup utility here to instantiate Ollama. Use the following
         // to set it up with your Ollama configuration.
         // Ollama ollama = new Ollama("http://your-ollama-host:11434/");
 
-        ollama.pullModel(model);
+        ollama.pullModel("mistral:7b");
     }
 }
