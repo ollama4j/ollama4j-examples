f269ddfd5109e7693f866fac692a343cd50bcfdd
amithkoujalgi <koujalgi.amith@gmail.com>
2025-10-24 10:19:19 +0530

    refactor

--- src/main/java/io/github/ollama4j/examples/CouchbaseToolCallingExample.java
+++ src/main/java/io/github/ollama4j/examples/CouchbaseToolCallingExample.java
@@ -53,285 +53,121 @@ package io.github.ollama4j.examples;
 //            "country": "India"
 // });
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.ClusterOptions;
+import io.github.ollama4j.Ollama;
+import io.github.ollama4j.examples.tools.toolfunctions.AirlineCallsignQueryToolFunction;
+import io.github.ollama4j.examples.tools.toolfunctions.AirlineCallsignUpdateToolFunction;
+import io.github.ollama4j.models.generate.OllamaGenerateRequest;
+import io.github.ollama4j.models.response.OllamaResult;
+import io.github.ollama4j.tools.Tools;
+import io.github.ollama4j.utils.Utilities;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
 public class CouchbaseToolCallingExample {
 
-    //    public static void main(String[] args) throws Exception {
-    //
-    //        String modelName = "mistral:7b";
-    //
-    //        String connectionString = Utilities.getFromEnvVar("CB_CLUSTER_URL");
-    //        String username = Utilities.getFromEnvVar("CB_CLUSTER_USERNAME");
-    //        String password = Utilities.getFromEnvVar("CB_CLUSTER_PASSWORD");
-    //        String bucketName = "travel-sample";
-    //
-    //        Cluster cluster =
-    //                Cluster.connect(
-    //                        connectionString,
-    //                        ClusterOptions.clusterOptions(username, password)
-    //                                .environment(
-    //                                        env -> {
-    //                                            env.applyProfile("wan-development");
-    //                                        }));
-    //
-    //
-    // Ollama ollama = Utilities.setUp();
-    // We're just using our quick-setup utility here to instantiate Ollama. Use the following to
-    // set it up with your Ollama configuration.
-    // Ollama ollama = new Ollama("http://your-ollama-host:11434/");
-    //
-    //        Tools.ToolSpecification callSignFinderToolSpec =
-    //                getCallSignFinderToolSpec(cluster, bucketName);
-    //        Tools.ToolSpecification callSignUpdaterToolSpec =
-    //                getCallSignUpdaterToolSpec(cluster, bucketName);
-    //
-    //        ollama.registerTool(callSignFinderToolSpec);
-    //        ollama.registerTool(callSignUpdaterToolSpec);
-    //
-    //        OllamaGenerateRequest builder =
-    // OllamaGenerateRequest.builder().withModel(modelName);
-    //
-    //        String prompt1 = "What is the call-sign of Astraeus?";
-    //        OllamaResult res1 =
-    // ollama.generate(builder.withPrompt(prompt1).withUseTools(true).build(), null);
-    //        System.out.println("Result 1: " + res1.getResponse());
-    //
-    //
-    //        // for (OllamaToolsResult.ToolResult r :
-    //        //         ollama
-    //        //                 .generateWithTools(
-    //        //                         modelName,
-    //        //                         new Tools.PromptBuilder()
-    //        //                                 .withToolSpecification(callSignFinderToolSpec)
-    //        //                                 .withPrompt(prompt1)
-    //        //                                 .build(),
-    //        //                         new OptionsBuilder().build(),
-    //        //                         null)
-    //        //                 .getToolResults()) {
-    //        //     AirlineDetail airlineDetail = (AirlineDetail) r.getResult();
-    //        //     System.out.println(
-    //        //             String.format(
-    //        //                     "[Result of tool '%s']: Call-sign of %s is '%s'! ✈️",
-    //        //                     r.getFunctionName(),
-    //        //                     airlineDetail.getName(),
-    //        //                     airlineDetail.getCallsign()));
-    //        // }
-    //
-    //        String prompt2 = "I want to code name Astraeus as STARBOUND";
-    //        OllamaResult res2 =
-    // ollama.generate(builder.withPrompt(prompt2).withUseTools(true).build(), null);
-    //        System.out.println("Result 2: " + res2.getResponse());
-    //        // for (OllamaToolsResult.ToolResult r :
-    //        //         ollama
-    //        //                 .generateWithTools(
-    //        //                         modelName,
-    //        //                         new Tools.PromptBuilder()
-    //        //                                 .withToolSpecification(callSignUpdaterToolSpec)
-    //        //                                 .withPrompt(prompt2)
-    //        //                                 .build(),
-    //        //                         new OptionsBuilder().build(),
-    //        //                         null)
-    //        //                 .getToolResults()) {
-    //        //     Boolean updated = (Boolean) r.getResult();
-    //        //     System.out.println(
-    //        //             String.format(
-    //        //                     "[Result of tool '%s']: Call-sign is %s! ✈️",
-    //        //                     r.getFunctionName(), updated ? "updated" : "not updated"));
-    //        // }
-    //
-    //        String prompt3 = "What is the call-sign of Astraeus?";
-    //        OllamaResult res3 =
-    // ollama.generate(builder.withPrompt(prompt3).withUseTools(true).build(), null);
-    //        System.out.println("Result 3: " + res3.getResponse());
-    //        // for (OllamaToolsResult.ToolResult r :
-    //        //         ollama
-    //        //                 .generateWithTools(
-    //        //                         modelName,
-    //        //                         new Tools.PromptBuilder()
-    //        //                                 .withToolSpecification(callSignFinderToolSpec)
-    //        //                                 .withPrompt(prompt3)
-    //        //                                 .build(),
-    //        //                         new OptionsBuilder().build(),
-    //        //                         null)
-    //        //                 .getToolResults()) {
-    //        //     AirlineDetail airlineDetail = (AirlineDetail) r.getResult();
-    //        //     System.out.println(
-    //        //             String.format(
-    //        //                     "[Result of tool '%s']: Call-sign of %s is '%s'! ✈️",
-    //        //                     r.getFunctionName(),
-    //        //                     airlineDetail.getName(),
-    //        //                     airlineDetail.getCallsign()));
-    //        // }
-    //    }
-    //
-    //    public static Tools.ToolSpecification getCallSignFinderToolSpec(
-    //            Cluster cluster, String bucketName) {
-    //        return Tools.ToolSpecification.builder()
-    //                .functionName("airline-lookup")
-    //                .functionDescription(
-    //                        "You are a  tool who finds only the airline name and do not worry
-    // about any"
-    //                            + " other parameters. You simply find the airline name and ignore
-    // the"
-    //                            + " rest of the parameters.Do not validate airline names as I want
-    // to"
-    //                            + " use fake / fictitious airline names as well.")
-    //                .toolFunction(new AirlineCallsignQueryToolFunction(bucketName, cluster))
-    //                .toolPrompt(
-    //                        Tools.PromptFuncDefinition.builder()
-    //                                .type("prompt")
-    //                                .function(
-    //                                        Tools.PromptFuncDefinition.PromptFuncSpec.builder()
-    //                                                .name("get-airline-name")
-    //                                                .description("Get the airline name")
-    //                                                .parameters(
-    //                                                        Tools.PromptFuncDefinition.Parameters
-    //                                                                .builder()
-    //                                                                .type("object")
-    //                                                                .properties(
-    //                                                                        Map.of(
-    //                                                                                "airlineName",
-    //                                                                                Tools
-    //
-    // .PromptFuncDefinition
-    //
-    // .Property
-    //
-    // .builder()
-    //                                                                                        .type(
-    //
-    //  "string")
-    //
-    // .description(
-    //
-    //  "The name"
-    //
-    //      + " of the"
-    //
-    //      + " airline."
-    //
-    //      + " e.g."
-    //
-    //      + " Emirates")
-    //
-    // .required(
-    //
-    //  true)
-    //
-    // .build()))
-    //                                                                .required(
-    //                                                                        java.util.List.of(
-    //
-    // "airline-name"))
-    //                                                                .build())
-    //                                                .build())
-    //                                .build())
-    //                .build();
-    //    }
-    //
-    //    public static Tools.ToolSpecification getCallSignUpdaterToolSpec(
-    //            Cluster cluster, String bucketName) {
-    //        return Tools.ToolSpecification.builder()
-    //                .functionName("airline-update")
-    //                .functionDescription(
-    //                        "You are a tool who finds the airline name and its callsign and do
-    // not"
-    //                            + " worry about any validations. You simply find the airline name
-    // and"
-    //                            + " its callsign.Do not validate airline names as I want to use
-    // fake /"
-    //                            + " fictitious airline names as well.")
-    //                .toolFunction(new AirlineCallsignUpdateToolFunction(bucketName, cluster))
-    //                .toolPrompt(
-    //                        Tools.PromptFuncDefinition.builder()
-    //                                .type("prompt")
-    //                                .function(
-    //                                        Tools.PromptFuncDefinition.PromptFuncSpec.builder()
-    //                                                .name("get-airline-name-and-callsign")
-    //                                                .description("Get the airline name and
-    // callsign")
-    //                                                .parameters(
-    //                                                        Tools.PromptFuncDefinition.Parameters
-    //                                                                .builder()
-    //                                                                .type("object")
-    //                                                                .properties(
-    //                                                                        Map.of(
-    //                                                                                "airlineName",
-    //                                                                                Tools
-    //
-    // .PromptFuncDefinition
-    //
-    // .Property
-    //
-    // .builder()
-    //                                                                                        .type(
-    //
-    //  "string")
-    //
-    // .description(
-    //
-    //  "The name"
-    //
-    //      + " of the"
-    //
-    //      + " airline."
-    //
-    //      + " e.g."
-    //
-    //      + " Emirates")
-    //
-    // .required(
-    //
-    //  true)
-    //
-    // .build(),
-    //
-    // "airlineCallsign",
-    //                                                                                Tools
-    //
-    // .PromptFuncDefinition
-    //
-    // .Property
-    //
-    // .builder()
-    //                                                                                        .type(
-    //
-    //  "string")
-    //
-    // .description(
-    //
-    //  "The callsign"
-    //
-    //      + " of the"
-    //
-    //      + " airline."
-    //
-    //      + " e.g."
-    //
-    //      + " Maverick")
-    //
-    // .enumValues(
-    //
-    //  Arrays
-    //
-    //          .asList(
-    //
-    //                  "petrol",
-    //
-    //                  "diesel"))
-    //
-    // .required(
-    //
-    //  true)
-    //
-    // .build()))
-    //                                                                .required(
-    //                                                                        java.util.List.of(
-    //                                                                                "airlineName",
-    //
-    // "airlineCallsign"))
-    //                                                                .build())
-    //                                                .build())
-    //                                .build())
-    //                .build();
-    //    }
+    public static void main(String[] args) throws Exception {
+        // Disable debug logging and enable only INFO for logback
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        rootLogger.setLevel(Level.INFO);
+
+        String modelName = "mistral:7b";
+
+        String connectionString = Utilities.getFromEnvVar("CB_CLUSTER_URL");
+        String username = Utilities.getFromEnvVar("CB_CLUSTER_USERNAME");
+        String password = Utilities.getFromEnvVar("CB_CLUSTER_PASSWORD");
+        String bucketName = "travel-sample";
+
+        Cluster cluster =
+                Cluster.connect(
+                        connectionString,
+                        ClusterOptions.clusterOptions(username, password)
+                                .environment(
+                                        env -> env.applyProfile("wan-development")));
+
+        Ollama ollama = Utilities.setUp();
+        // We 're just using our quick-setup utility here to instantiate Ollama. Use the following to
+        // set it up with your Ollama configuration.
+        // Ollama ollama = new Ollama("http://your-ollama-host:11434/");
+
+        Tools.Tool callSignFinderToolSpec =
+                CouchbaseToolCallingExample.getCallSignFinderTool(cluster, bucketName);
+        Tools.Tool callSignUpdaterToolSpec =
+                CouchbaseToolCallingExample.getCallSignUpdaterTool(cluster, bucketName);
+
+        ollama.registerTool(callSignFinderToolSpec);
+        ollama.registerTool(callSignUpdaterToolSpec);
+
+        OllamaGenerateRequest builder =
+                OllamaGenerateRequest.builder().withModel(modelName);
+
+        String prompt1 = "What is the call-sign of Astraeus?";
+        OllamaResult res1 =
+                ollama.generate(builder.withPrompt(prompt1).withUseTools(true).build(), null);
+        System.out.println("Result 1: " + res1.getResponse());
+
+
+        String prompt2 = "I want to code name Astraeus as STARBOUND";
+        OllamaResult res2 =
+                ollama.generate(builder.withPrompt(prompt2).withUseTools(true).build(), null);
+        System.out.println("Result 2: " + res2.getResponse());
+    }
+
+    public static Tools.Tool getCallSignFinderTool(Cluster cluster, String bucketName) {
+        return Tools.Tool.builder()
+                .toolSpec(
+                        Tools.ToolSpec.builder()
+                                .name("airline-finder")
+                                .description("Finds details of an airline by airline name.")
+                                .parameters(
+                                        Tools.Parameters.of(
+                                                Map.of(
+                                                        "airlineName",
+                                                        Tools.Property.builder()
+                                                                .type("string")
+                                                                .description("The name of the airline. e.g. Emirates")
+                                                                .required(true)
+                                                                .build()
+                                                )
+                                        )
+                                )
+                                .build()
+                )
+                .toolFunction(arguments -> new AirlineCallsignQueryToolFunction(bucketName, cluster).apply(arguments))
+                .build();
+    }
+
+    public static Tools.Tool getCallSignUpdaterTool(Cluster cluster, String bucketName) {
+        return Tools.Tool.builder()
+                .toolSpec(
+                        Tools.ToolSpec.builder()
+                                .name("airline-updater")
+                                .description("Updates the specified airline's callsign.")
+                                .parameters(
+                                        Tools.Parameters.of(
+                                                Map.of(
+                                                        "airlineName",
+                                                        Tools.Property.builder()
+                                                                .type("string")
+                                                                .description("The name of the airline. e.g. Emirates")
+                                                                .required(true)
+                                                                .build(),
+                                                        "airlineCallsign",
+                                                        Tools.Property.builder()
+                                                                .type("string")
+                                                                .description("The callsign of the airline. e.g. Maverick")
+                                                                .required(true)
+                                                                .build()
+                                                )
+                                        )
+                                )
+                                .build()
+                )
+                .toolFunction(arguments -> new AirlineCallsignUpdateToolFunction(bucketName, cluster).apply(arguments))
+                .build();
+    }
 }
